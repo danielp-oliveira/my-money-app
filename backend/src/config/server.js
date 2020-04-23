001f54bfd46d795b39266beecdd024364b1c74fb
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-04-23 19:09:31 -0300

    Criando API REST e Mapeando Rotas

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,7 +1,7 @@
-require("dotenv").config()
+require('dotenv').config()
 
-const bodyParser = require("body-parser")
-const express = require("express")
+const bodyParser = require('body-parser')
+const express = require('express')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
@@ -9,3 +9,5 @@ server.use(bodyParser.json())
 server.listen(process.env.PORT, () => {
   console.log(`BACKEND is running on port ${process.env.PORT}.`)
 })
+
+module.exports = server
